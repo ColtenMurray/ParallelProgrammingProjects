831f2ec1139ad2d3d71ee923508eea0649649b9d
ColtenMurray <60444923+ColtenMurray@users.noreply.github.com>
2021-05-19 09:45:20 -0500

    Add files via upload

---
+++ OSProject2/project2.cpp
@@ -0,0 +1,275 @@
+#include <cstdlib>
+#include <cstdio>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <fstream>
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+static int seed;
+static bool isGameOver;
+static int DiceSum_A;
+static int DiceSum_B;
+static int DiceSum_C;
+static int DiceSum_D;
+
+
+
+pthread_mutex_t winning_mutex;
+pthread_mutex_t A_mutex;
+pthread_mutex_t B_mutex;
+pthread_mutex_t C_mutex;
+pthread_mutex_t D_mutex;
+
+pthread_cond_t game_won;
+pthread_cond_t A_turn;
+pthread_cond_t B_turn;
+pthread_cond_t C_turn;
+pthread_cond_t D_turn;
+
+using namespace std;
+
+static void* rollDice(void* arg)
+{
+    const long my_rank = (long)arg;
+    printf("thread %d running rollDice\n", (int)my_rank);
+
+    int DiceRoll_1;
+    int DiceRoll_2;
+    int DiceSum;
+    ofstream output;
+
+    while(!isGameOver)
+    {
+        switch(my_rank)
+        {
+        case 0:
+            pthread_mutex_lock(&A_mutex);
+            pthread_cond_wait(&A_turn,&A_mutex);
+
+
+            if(!isGameOver)
+            {
+
+                DiceRoll_1 = rand()%6 + 1;
+                DiceRoll_2 = rand()%6 + 1;
+                DiceSum = DiceRoll_1 + DiceRoll_2;
+                DiceSum_A = DiceSum;
+
+                output.open("output.txt", ios::out | ios::app);
+                output << "PLAYER A: gets " << DiceRoll_1 << " and " << DiceRoll_2 << " with a sum " << DiceSum << "\n";
+                output.close();
+                printf("PLAYER A: %d %d \n",DiceRoll_1,DiceRoll_2);
+
+                if(DiceSum_A == DiceSum_C)
+                {
+                    isGameOver = true;
+                    pthread_mutex_lock(&winning_mutex);
+                    pthread_cond_signal(&game_won);
+                    pthread_mutex_unlock(&winning_mutex);
+                }
+            }
+
+            pthread_mutex_lock(&B_mutex);
+            pthread_cond_signal(&B_turn);
+            pthread_mutex_unlock(&B_mutex);
+
+            pthread_mutex_unlock(&A_mutex);
+
+            break;
+
+        case 1:
+            pthread_mutex_lock(&B_mutex);
+            pthread_cond_wait(&B_turn,&B_mutex);
+
+            if(!isGameOver)
+            {
+                DiceRoll_1 = rand()%6 + 1;
+                DiceRoll_2 = rand()%6 + 1;
+                DiceSum = DiceRoll_1 + DiceRoll_2;
+                DiceSum_B = DiceSum;
+
+                output.open("output.txt", ios::out | ios::app);
+                output << "PLAYER B: gets " << DiceRoll_1 << " and " << DiceRoll_2 << " with a sum " << DiceSum << "\n";
+                output.close();
+                printf("PLAYER B: %d %d \n",DiceRoll_1,DiceRoll_2);
+
+                if(DiceSum_B == DiceSum_D)
+                {
+                    isGameOver = true;
+                    pthread_mutex_lock(&winning_mutex);
+                    pthread_cond_signal(&game_won);
+                    pthread_mutex_unlock(&winning_mutex);
+                }
+            }
+
+            pthread_mutex_lock(&C_mutex);
+            pthread_cond_signal(&C_turn);
+            pthread_mutex_unlock(&C_mutex);
+
+            pthread_mutex_unlock(&B_mutex);
+            break;
+        case 2:
+            pthread_mutex_lock(&C_mutex);
+            pthread_cond_wait(&C_turn,&C_mutex);
+
+            if(!isGameOver)
+            {
+
+                DiceRoll_1 = rand()%6 + 1;
+                DiceRoll_2 = rand()%6 + 1;
+                DiceSum = DiceRoll_1 + DiceRoll_2;
+                DiceSum_C = DiceSum;
+
+                output.open("output.txt", ios::out | ios::app);
+                output << "PLAYER C: gets " << DiceRoll_1 << " and " << DiceRoll_2 << " with a sum " << DiceSum << "\n";
+                output.close();
+                printf("PLAYER C: %d %d\n",DiceRoll_1,DiceRoll_2);
+
+                if(DiceSum_A == DiceSum_C)
+                {
+                    isGameOver = true;
+                    pthread_mutex_lock(&winning_mutex);
+                    pthread_cond_signal(&game_won);
+                    pthread_mutex_unlock(&winning_mutex);
+                }
+            }
+
+            pthread_mutex_lock(&D_mutex);
+            pthread_cond_signal(&D_turn);
+            pthread_mutex_unlock(&D_mutex);
+
+            pthread_mutex_unlock(&C_mutex);
+            break;
+        case 3:
+            pthread_mutex_lock(&D_mutex);
+            pthread_cond_wait(&D_turn,&D_mutex);
+
+            if(!isGameOver)
+            {
+
+                DiceRoll_1 = rand()%6 + 1;
+                DiceRoll_2 = rand()%6 + 1;
+                DiceSum = DiceRoll_1 + DiceRoll_2;
+                DiceSum_D = DiceSum;
+
+                output.open("output.txt", ios::out | ios::app);
+                output << "PLAYER D: gets " << DiceRoll_1 << " and " << DiceRoll_2 << " with a sum " << DiceSum << "\n";
+                output.close();
+                printf("PLAYER D: %d %d\n",DiceRoll_1,DiceRoll_2);
+
+                if(DiceSum_B == DiceSum_D)
+                {
+                    isGameOver = true;
+                    pthread_mutex_lock(&winning_mutex);
+                    pthread_cond_signal(&game_won);
+                    pthread_mutex_unlock(&winning_mutex);
+                }
+            }
+
+            pthread_mutex_lock(&A_mutex);
+            pthread_cond_signal(&A_turn);
+            pthread_mutex_unlock(&A_mutex);
+
+            pthread_mutex_unlock(&D_mutex);
+            break;
+        }
+    }
+    pthread_exit(NULL);
+}
+
+static void* deal(void* arg)
+{
+    ofstream output;
+    const long my_rank = (long)arg;
+    printf("thread %d running deal\n", (int)my_rank);
+
+    pthread_mutex_lock(&winning_mutex);
+
+    pthread_mutex_lock(&A_mutex);
+    pthread_cond_signal(&A_turn);
+    pthread_mutex_unlock(&A_mutex);
+
+    pthread_cond_wait(&game_won,&winning_mutex);
+    if(DiceSum_A == DiceSum_C)
+    {
+        output.open("output.txt", ios::out | ios::app);
+        output << "DEALER: The winning team is A and C\n";
+        output.close();
+        printf("DEALER: The winning team is A and C\n");
+    }
+    else
+    {
+        output.open("output.txt", ios::out | ios::app);
+        output << "DEALER: The winning team is B and D\n";
+        output.close();
+
+        printf("DEALER: The winning team is B and D\n");
+    }
+    pthread_mutex_unlock(&winning_mutex);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char* argv[])
+{
+
+  // check command line
+  if (argc != 2) {fprintf(stderr, "USAGE: %s seed\n", argv[0]); exit(-1);}
+  seed = atoi(argv[1]);
+  srand(seed);
+
+  ofstream output;
+  output.open("output.txt",ios::out | ios::trunc);
+  output << "Matching Dice!\n";
+  output.close();
+  printf("Matching Dice!\n");
+
+  long threads = 5;
+  isGameOver = false;
+
+
+
+  // initialize pthread variables
+  pthread_t* const handle = new pthread_t [threads];
+
+  // launch threads
+  for(int thread = 0; thread < threads -1; thread++)
+  {
+      if( pthread_create(&handle[thread], NULL, rollDice, (void *)thread) != 0)
+      {
+          printf("Error creating Player thread %d...\n", thread);
+      }
+  }
+  sleep(1);
+  if(pthread_create(&handle[threads-1], NULL, deal, (void *)threads-1) != 0)
+  {
+      printf("Error creating Dealer thread...\n");
+  }
+
+
+  // join threads
+  for(int thread = 0; thread < threads; thread++)
+  {
+      pthread_join(handle[thread], NULL);
+  }
+
+  output.close();
+  // cleanup
+  pthread_mutex_destroy(&winning_mutex);
+  pthread_mutex_destroy(&A_mutex);
+  pthread_mutex_destroy(&B_mutex);
+  pthread_mutex_destroy(&C_mutex);
+  pthread_mutex_destroy(&D_mutex);
+  pthread_cond_destroy(&game_won);
+  pthread_cond_destroy(&A_turn);
+  pthread_cond_destroy(&B_turn);
+  pthread_cond_destroy(&C_turn);
+  pthread_cond_destroy(&D_turn);
+  pthread_exit(NULL);
+
+}
---
+++ Project3/collatz_pthread.cpp
@@ -0,0 +1,110 @@
+/*
+Collatz code for CS 4380 / CS 5351
+
+Copyright (c) 2020 Texas State University. All rights reserved.
+
+Redistribution in source or binary form, with or without modification,
+is *not* permitted. Use in source or binary form, with or without
+modification, is only permitted for academic use in CS 4380 or CS 5351
+at Texas State University.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Author: Martin Burtscher
+*/
+
+#include <cstdio>
+#include <algorithm>
+#include <pthread.h>
+#include <sys/time.h>
+
+// shared variables
+static long threads;
+static long bound;
+static int maxlen;
+static pthread_once_t once = PTHREAD_ONCE_INIT;
+static pthread_mutex_t mutex;
+
+
+static void* collatz(void* arg)
+{
+  const long my_rank = (long)arg;
+  // compute sequence lengths
+  int local_len = 0;
+
+  for (long i = my_rank + 1; i <= bound; i += threads) {
+    long val = i;
+    int len = 1;
+    while (val != 1) {
+      len++;
+      if ((val % 2) == 0) {
+        val /= 2;  // even
+      } else {
+        val = 3 * val + 1;  // odd
+      }
+    }
+    local_len = std::max( local_len, len);
+  }
+  pthread_mutex_lock(&mutex);
+  maxlen = std::max(maxlen, local_len);
+  pthread_mutex_unlock(&mutex);
+  return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+  printf("Collatz v1.4\n");
+
+  // check command line
+  if (argc != 3) {fprintf(stderr, "USAGE: %s upper_bound\n threads", argv[0]); exit(-1);}
+  bound = atol(argv[1]);
+  if (bound < 1) {fprintf(stderr, "ERROR: upper_bound must be at least 1\n"); exit(-1);}
+  printf("upper bound: %ld\n", bound);
+  threads = atol(argv[2]);
+  if (threads < 1) {fprintf(stderr, "ERROR: number_of_threads must be at least 1\n"); exit(-1);}
+  printf("threads: %ld\n", threads);
+
+    // initialize pthread variables
+  pthread_mutex_init(&mutex,NULL);
+
+  pthread_t* const handle = new pthread_t [threads - 1];
+
+
+
+  // start time
+  timeval start, end;
+  gettimeofday(&start, NULL);
+
+  // execute timed code
+
+  // launch threads
+  for (long thread = 0; thread < threads - 1; thread++)
+    pthread_create(&handle[thread], NULL, collatz, (void *)thread);
+
+    // work for master
+  collatz((void*)(threads - 1));
+
+  // join threads
+  for (long thread = 0; thread < threads - 1; thread++)
+    pthread_join(handle[thread], NULL);
+
+
+  // end time
+  gettimeofday(&end, NULL);
+  const double runtime = end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec) / 1000000.0;
+  printf("compute time: %.6f s\n", runtime);
+  pthread_mutex_destroy(&mutex);
+
+  // print result
+  printf("longest sequence length: %d elements\n", maxlen);
+  return 0;
+}
